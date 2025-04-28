c45939666a792925563dfe840978301de3eaf1ae
MarceloHeZhen <mhezhen@frba.utn.edu.ar>
2025-04-28 12:31:40 -0300

    Subo lo que hice en el labo

---
+++ src/funcional-braulioestrella.hs
@@ -0,0 +1,10 @@
+data Personaje = unPersonaje {
+    nombre :: String,
+    poderBasico :: String,
+    superPoder :: String,
+    superPoderActivo :: Bool,
+    cantidadDeVida :: Int,
+}
+
+Espina :: Personaje
+Pamela :: Personaje
